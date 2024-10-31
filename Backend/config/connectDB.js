7ea343edc544fef3f4fab0e0f1e4632cae2b2c78
mohdameenmca <169062410+mohdameenmca@users.noreply.github.com>
2024-10-31 22:27:03 +0530

    Update connectDB.js

--- Backend/config/connectDB.js
+++ Backend/config/connectDB.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 
  const connectDB=()=>{
-    mongoose.connect(process.env.DB_URL).then((con)=>{
+    mongoose.connect(process.env.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then((con)=>{
         console.log("Mongo DB connected successfully on host :"+con.connection.host);
     })
 }
